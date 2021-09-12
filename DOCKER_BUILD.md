6f4eee9b7ba064dad2327f11b3c252c2411ab3b8
Pierre-Luc Paour <github@paour.com>
2021-09-12 17:08:07 +0200

    Fix cron definition

---
+++ DOCKER_BUILD.md
@@ -0,0 +1,6 @@
+## Create docker image
+
+```shell
+ docker build -t paour/flickr_email .
+ docker push paour/flickr_email:latest
+```
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-echo "$CRONTAB python /app/flick_email.py" > /etc/cron.d/crontab
+echo "$CRONTAB python /app/flickr_email.py" > /etc/cron.d/crontab
 chmod 0644 /etc/cron.d/crontab
 /usr/bin/crontab /etc/cron.d/crontab
 
