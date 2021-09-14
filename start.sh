23f52c2f2914ed197bdf02cd9e30882a9e0ceac4
Pierre-Luc Paour <github@paour.com>
2021-09-14 08:06:31 +0200

    Fix cron file

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-echo "$CRONTAB python /app/flickr_email.py" > /etc/cron.d/crontab
-chmod 0644 /etc/cron.d/crontab
-/usr/bin/crontab /etc/cron.d/crontab
+echo "$CRONTAB root python /app/flickr_email.py" > /etc/cron.d/flickr_email
+chmod 0644 /etc/cron.d/flickr_email
+/usr/bin/crontab /etc/cron.d/flickr_email
 
 cron -f
